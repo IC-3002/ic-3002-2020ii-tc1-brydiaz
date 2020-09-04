98ebd18484c1b4d8474abc2e8838271ce50badf4
brydiaz <53624940+brydiaz@users.noreply.github.com>
2020-09-03 22:48:16 -0600

    Add files via upload

--- euler.py
+++ euler.py
@@ -1,8 +1,28 @@
 def e_cuadratica(n):
     # Implemente esta función
-    return 0
+    i=0
+    e=0
+    while(i!=n):
+        e=e+1/factorial(i)
+        i+=1
+    return e
 
 
 def e_lineal(n):
     # Implemente esta función
-    return 0
+    i=0
+    factorial=1
+    e=0
+    while(i!=n):
+        e=e+1/factorial
+        i+=1
+        factorial=factorial*i
+    return e
+
+
+def factorial(num):
+    factorial=1
+    while(num>0):
+        factorial=factorial*num
+        num=num-1
+    return factorial 
